35a414f89cb9ea279e00b6523577640d0d454f38
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2024-01-21 15:57:58 +0000

    css

---
+++ script.js
@@ -0,0 +1,13 @@
+//selecioanr elementos
+
+const inputElement = document.querySelector("#input");
+const fromElement = document.querySelector("#from");
+const toElement = document.querySelector("#to");
+const outputElement = document.querySelector("#output");
+const messageElement = document.querySelector("#message");
+const convertButton = document.querySelector("#convert-btn");
+
+//conversao
+
+
+
