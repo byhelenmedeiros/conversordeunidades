3c65f8421ddf89cd1985cfaf44de7b0abb8acfa7
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2024-01-21 17:04:06 +0000

    corregit script

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function convert() {
         return;
     }
 
-    // Converter para metros
+    // Converter 
     let metros;
     switch (fromValue) {
         case "m":
@@ -36,115 +36,21 @@ function convert() {
             break;
     }
 
-    let resultMetros;
+    let result;
     switch (toValue) {
         case "m":
-            resultMetros = metros;
+            result = metros;
             break;
         case "km":
-            resultMetros = metros / 1000;
+            result = metros / 1000;
             break;
         case "cm":
-            resultMetros = metros * 100;
+            result = metros * 100;
             break;
         case "mm":
-            resultMetros = metros * 1000;
+            result = metros * 1000;
             break;
     }
-    // Converter para quilometros
-    let km;
-    switch (fromValue) {
-        case "m":
-            km = parseFloat(inputElement.value);
-            break;
-        case "km":
-            km = parseFloat(inputElement.value) * 1000;
-            break;
-        case "cm":
-            km = parseFloat(inputElement.value) / 100;
-            break;
-        case "mm":
-            km = parseFloat(inputElement.value) / 1000;
-            break;
-    }
-
-    let resultKM;
-    switch (toValue) {
-        case "m":
-            resultKM = km;
-            break;
-        case "km":
-            resultKM = km / 1000;
-            break;
-        case "cm":
-            resultKM = km * 100;
-            break;
-        case "mm":
-            resultKM = km * 1000;
-            break;
-    }
-        let cm;
-        switch (fromValue) {
-            case "m":
-                cm = parseFloat(inputElement.value);
-                break;
-            case "km":
-                cm = parseFloat(inputElement.value) * 1000;
-                break;
-            case "cm":
-                cm = parseFloat(inputElement.value) / 100;
-                break;
-            case "mm":
-                cm = parseFloat(inputElement.value) / 1000;
-                break;
-        }
-
-        let resultCM;
-        switch (toValue) {
-            case "m":
-                resultCM = cm;
-                break;
-            case "km":
-                resultCM = cm / 1000;
-                break;
-            case "cm":
-                resultCM = cm * 100;
-                break;
-            case "mm":
-                resultCM = cm * 1000;
-                break;
-        }
-        let mm;
-        switch (fromValue) {
-            case "m":
-                mm = parseFloat(inputElement.value);
-                break;
-            case "km":
-                mm = parseFloat(inputElement.value) * 1000;
-                break;
-            case "cm":
-                mm = parseFloat(inputElement.value) / 100;
-                break;
-            case "mm":
-                mm = parseFloat(inputElement.value) / 1000;
-                break;
-        }
-
-        let resultMM;
-        switch (toValue) {
-            case "m":
-                resultMM = mm;
-                break;
-            case "km":
-                resultMM = mm / 1000;
-                break;
-            case "cm":
-                resultMM = mm * 100;
-                break;
-            case "mm":
-                resultMM = mm * 1000;
-                break;
-        }
 
     outputElement.value = result;
     const message = `${inputElement.value} ${fromLabel} equivale a ${result} ${toLabel}`;
