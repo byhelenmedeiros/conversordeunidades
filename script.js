d3464fd3c7def49dbced1c3a094d3797791a8d69
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2024-01-21 16:39:50 +0000

    correcao js

--- script.js
+++ script.js
@@ -9,5 +9,52 @@ const convertButton = document.querySelector("#convert-btn");
 
 //conversao
 
+function convert(){
+    const fromValue = fromElement.value;
+    const toValue = toElement.value;
+
+    if (fromValue === toValue){
+        outputElement.value = inputElement.value;
+        messageElement.textContent = "";
+        return;
+    }
+
+    //converter para metros
+    let metros;
+        switch (fromValue) {
+            case "m":
+                metros = inputElement.value;
+                break;
+            case "km":
+                metros = inputElement.value * 1000; 
+                break;
+            case "cm":
+                metros = inputElement.value / 100;
+                break;
+            case "mm":
+                metros = inputElement.value /1000;
+                break;   
+        }
+
+    let result;
+    switch (toValue) {
+        case "m":
+            result = metros;
+            break;
+        case "km":
+            result = metros / 1000; 
+            break;
+        case "cm":
+            result = metros * 100;
+            break;
+        case "mm":
+            result = metros * 1000;
+            break;   
+    }
+        outputElement.value = result;
+    const fromLabel = fromElement.options[fromElement.selectedIndex].text;
+    const toLabel = fromElement.options[toElement.selectedIndex].text;
 
 
+}
+convertButton.addEventListener("click", convert)
